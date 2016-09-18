23b7089093b5c267aeef0e1ffb127647997eeee6
Nati Namvong <nati@flyingcomma.com>
2016-09-18 16:02:05 +0700

    bulma

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@
       <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
       <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
     </p>
+    <button class="button is-primary is-large is-loading">button</button>
   </div>
 </template>
 
@@ -33,34 +34,5 @@ export default {
 </script>
 
 <style lang="scss">
-$xcolor: #0FF;
-
-html {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: $xcolor;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
+@import '~bulma';
 </style>
