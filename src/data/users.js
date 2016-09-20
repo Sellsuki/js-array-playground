bd00ed210f68a13219d02a65bb667362a8ad68c2
Nati Namvong <nati@flyingcomma.com>
2016-09-20 22:45:12 +0700

    add users data

---
+++ src/data/users.js
@@ -0,0 +1,38 @@
+let users = [
+  {
+    id: 1000,
+    name: 'Corey Griffith',
+    username: 'coGriffith',
+    gender: 'male',
+    age: 13
+  },
+  {
+    id: 1001,
+    name: 'Marion McDaniel',
+    username: 'mMcDaniel',
+    gender: 'female',
+    age: 15
+  },
+  {
+    id: 1002,
+    name: 'Tom Robbins',
+    username: 'tRobbins',
+    gender: 'male',
+    age: 15
+  },
+  {
+    id: 1003,
+    name: 'Ruth Harvey',
+    username: 'rHarvey',
+    gender: 'female',
+    age: 14
+  },
+  {
+    id: 1004,
+    name: 'Terry Willis',
+    username: 'tWillis',
+    gender: 'male',
+    age: 13
+  }
+]
+export default users
