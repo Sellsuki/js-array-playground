05eb6c9ddc2b72b0a52524202b073430c80bd09f
Jirayu Laungwilawan <jirayu.l@ku.th>
2018-05-31 18:20:05 +0700

    Add change web icon and og image

--- pages/index.vue
+++ pages/index.vue
@@ -40,6 +40,15 @@ import users from '../data/users'
 import products from '../data/products'
 
 export default {
+  head: {
+    titleTemplate: '%s - Nuxt.js',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Meta description' },
+      { property: 'og:image', content: "../static/preview.jpg" }
+    ]
+  },
   data () {
     return {
       isTop: true,
