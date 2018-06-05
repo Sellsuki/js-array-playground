319a58c24477eba99c71b7af913dc25b60a059ec
Chanwit Piromplad <kingkong2103@gmail.com>
2018-06-05 10:43:20 +0700

    add content && can use console.log() in example

--- components/ContentFooter.vue
+++ components/ContentFooter.vue
@@ -1,8 +1,9 @@
 <template>
   <center class="is-small">
-    <a class="github-button" href="https://github.com/Sellsuki/js-array-playground" data-style="mega" data-count-href="/Sellsuki/js-array-playground/stargazers" data-count-api="/repos/Sellsuki/js-array-playground#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star Sellsuki/js-array-playground on GitHub">Star</a>
+    <a class="github-button" href="https://github.com/Sellsuki/js-array-playground" data-size="large" data-count-href="/Sellsuki/js-array-playground/stargazers" data-show-count="true" data-count-aria-label="# stargazers on GitHub" aria-label="Star Sellsuki/js-array-playground on GitHub">Star</a>
     &nbsp;&nbsp;
-    <a class="github-button" href="https://github.com/Sellsuki/js-array-playground/fork" data-icon="octicon-repo-forked" data-style="mega" data-count-href="/Sellsuki/js-array-playground/network" data-count-api="/repos/Sellsuki/js-array-playground#forks_count" data-count-aria-label="# forks on GitHub" aria-label="Fork Sellsuki/js-array-playground on GitHub">Fork</a>
+    <a class="github-button" href="https://github.com/Sellsuki/js-array-playground/fork" data-icon="octicon-repo-forked" data-size="large" data-count-href="/Sellsuki/js-array-playground/#forks_count" 
+    data-show-count="true" data-count-aria-label="# forks on GitHub" aria-label="Fork Sellsuki/js-array-playground on GitHub">Fork</a>
     <br><br>
     Made with {{ footerEmoji }} and <a href="http://vuejs.org" target="_blank"><img class="vue-icon" src="https://vuejs.org/images/logo.png" alt="vue.js"/></a>.
     <br>
